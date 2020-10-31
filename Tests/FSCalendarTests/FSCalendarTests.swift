657de279b8e6c7442cfc41884044e02ce14a72c9
Matt Ozer <matt@mattozer.com>
2020-10-31 07:59:21 -0700

    Update package

--- Tests/FSCalendarTests/FSCalendarTests.swift
+++ Tests/FSCalendarTests/FSCalendarTests.swift
@@ -6,7 +6,6 @@ final class FSCalendarTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(FSCalendar().text, "Hello, World!")
     }
 
     static var allTests = [
